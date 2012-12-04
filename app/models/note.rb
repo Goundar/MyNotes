398d7b9af7cbb0b9718d748da7aa0a5ab9e9fb5d
Matthieu Segret <matthieu.segret@gmail.com>
2012-12-04 18:08:44 +0100

    Implémentation de la méthode de recherche

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,7 @@
 class Note < ActiveRecord::Base
   attr_accessible :content, :title
+
+  def self.search(keywords)
+    where('lower(title) like :keywords OR lower(content) like :keywords', :keywords => "%#{keywords.downcase}%")
+  end
 end
