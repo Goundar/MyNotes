8c449367981f64203d097615b95a3396d003009f
Matthieu Segret <matthieu.segret@gmail.com>
2012-12-04 15:54:58 +0100

    Création du projet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyNotes::Application.config.secret_token = 'bca39111bf33f860cfe55d37b53ce7ede607b6d1d236ee45de6275ee1c485f71cf9ea758b8a3c3c1b16c0ab548a5fef53b2b66896b9da62684ed2a283d3216a6'
