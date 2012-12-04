b24c76103a0f40a348445a5ded366ed0401338cf
Matthieu Segret <matthieu.segret@gmail.com>
2012-12-04 18:07:37 +0100

    Mise en place d'un test unitaire pour tester future une méthode de recherche…

--- test/unit/note_test.rb
+++ test/unit/note_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "search a note" do
+    notes = Note.search("matz")
+    assert_equal 1, notes.length
+    assert_equal "Matz", notes[0].title
+    assert_equal "I love Ruby !", notes[0].content
+  end
 end
