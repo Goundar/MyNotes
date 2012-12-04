d8db4b44b5975a138a58eea1bf2717e351be16bf
Matthieu Segret <matthieu.segret@gmail.com>
2012-12-04 18:23:49 +0100

    Ajout de règles de validation

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,9 @@
 class Note < ActiveRecord::Base
   attr_accessible :content, :title
 
+  validates_presence_of :content, :title
+  validates_length_of :content, :maximum => 50
+
   def self.search(keywords)
     where('lower(title) like :keywords OR lower(content) like :keywords', :keywords => "%#{keywords.downcase}%")
   end
